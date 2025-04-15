60014cab2777105e3eff7d93083716010c71e01c
HrdHat <hrdhatpawel@gmail.com>
2025-04-15 14:36:37 -0700

    updated home buttons and added icons

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
 import "../modules/modules.css"; // Make sure the CSS is applied
+import SettingIcon from "../assets/settings_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
+import NewFormIcon from "../assets/note_add_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
+import SavedFormsIcon from "../assets/home_storage_24dp_1F1F1F_FILL0_wght400_GRAD0_opsz24.svg";
 
 const HomePage = () => {
   return (
     <div>
       <div className="home-button-container">
         <Link to="/flra" className="start-flra-button">
-          📝 Start new FLRA form
+          <img src={NewFormIcon} alt="New Form" className="icon" />
+          Start new FLRA form
         </Link>
         <Link to="/saved" className="home-button">
-          📂 View Saved Forms
+          <img src={SavedFormsIcon} alt="New Form" className="icon" />
+          View Saved Forms
         </Link>
         <Link to="/settings" className="home-button">
-          ⚙️ Account Settings
+          <img src={SettingIcon} alt="New Form" className="icon" />
+          Account Settings
         </Link>
       </div>
     </div>
